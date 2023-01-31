92a47c7129d6df075ca6375f37424587882231de
ulugbek-29-coder <ulugbek1993uzb@gmail.com>
2023-01-31 15:56:31 +0100

    created privated route and custom hook for protecting the profile page and logout functionality added

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,63 @@
+import { getAuth } from "firebase/auth";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 function Profile() {
-  return <div>Profile</div>;
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
+  const { name, email } = formData;
+
+  function onLogout() {
+    // The user will be logged out & navigated to the home page!
+    auth.signOut();
+    navigate("/");
+  }
+
+  return (
+    <>
+      <section className="max-w-7xl mx-auto flex justify-center items-center flex-col">
+        <h1 className="text-4xl text-center mt-6 font-bold">My Profile</h1>
+        <div className="w-full mt-6 px-3 md:w-[50%]">
+          <form>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              disabled
+              className="w-full mb-6 px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+            />
+
+            <input
+              type="email"
+              id="email"
+              value={email}
+              disabled
+              className="w-full mb-6 px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+            />
+
+            <div className="flex justify-between whitespace-nowrap mb-6">
+              <p>
+                Do you want to change your name?{" "}
+                <span className="text-red-600 hover:text-red-700 transition duration-200 ease-in-out cursor-pointer">
+                  Edit.
+                </span>
+              </p>
+              <p
+                onClick={onLogout}
+                className="text-blue-600 hover:text-blue-700 transition duration-200 ease-in-out cursor-pointer"
+              >
+                Sign out.
+              </p>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Profile;
